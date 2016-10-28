5ec2ee208fc9aaad695b66028cfb9f0eaa76259b
MatthiasSaihttam <matthiascp@gmail.com>
2016-10-27 19:13:18 -0700

    Added some code for it to run and kept the results of testing said code.

--- bash-to-run.sh
+++ bash-to-run.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-date >> /hotlist-snapshots/snapshots/test.txt
+# Create a date variable. Remove a bunch to make it look nice.
+dayVar=$(date -u)
+dayVar=${dayVar:4} #Cut off the weekday
+dayVar=${dayVar// /_} #Replace spaces with underscores, since it's going to be a filename
+
+#Create a variable for the hotlist's API path
+HLpath="https://www.khanacademy.org/api/internal/scratchpads/top?sort=3&limit=50&topic_id=xffde7c31&callback=snapshot"
+
+fileName="${dayVar}.js"
+
+curl -o $fileName $HLpath #dowload the data into a .js file named for the timestamp
+
+mv $fileName snapshots
+
+#date >> /hotlist-snapshots/snapshots/test.txt
