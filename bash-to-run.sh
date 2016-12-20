cd00ae7c1cca2585e0b7f3da4751c2781e1c4c18
Matthias Saihttam <MatthiasSaihttam1@gmail.com>
2016-12-20 10:19:49 -0800

    Simplified bash-to-run and removed some test code.

--- bash-to-run.sh
+++ bash-to-run.sh
@@ -11,24 +11,18 @@ fileName="${dayVar}.js"
 #Create a variable for the hotlist's API path
 HLpath="https://www.khanacademy.org/api/internal/scratchpads/top?sort=3&limit=50&topic_id=xffde7c31&callback=snapshot"
 
+cd snapshots
 #Copy the hotlist data and put it into a .js file named for the timestamp
 curl -so $fileName $HLpath
-#echo $dayVar > $fileName
-
-#move the file to the snapshots folder
-mv $fileName snapshots
+contents=$(<$fileName)
 
 #Create a list of all the snapshots
-cd snapshots
 allSnapshots=$(ls)
 allSnapshots=${allSnapshots//$'\n'/$'\",\n\"'} #Replace line breaks with ",\n"
 allSnapshots="/**/allSnapshots(\"${allSnapshots}\")" #Pass all the snapshots to a function called allSnapshots
+cd ..
 echo "$allSnapshots" > allSnapshots.js #Put it into a .js file for easy access
 
-mv allSnapshots.js ..
-
-contents=$(<$fileName)
-cd ..
 allSnapshotData=$(<"allSnapshotData.js")
 contents=${contents:12}
 
